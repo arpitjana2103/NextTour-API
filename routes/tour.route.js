65bfc43c3a2bb42232bd7dd334b55b3e236ad38b
Arpit Jana <arpitjana2103@gmail.com>
2025-05-03 08:25:52 +0530

    createTour controller attached

--- routes/tour.route.js
+++ routes/tour.route.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const tourController = require("../controllers/tour.controller");
 
 const tourRouter = express.Router();
+
+tourRouter.route("/").get(tourController.createTour);
 
 module.exports = tourRouter;
